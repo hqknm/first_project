0268f357942eea6c8d006f7b805575342367ad78
hqknm <158313959+hqknm@users.noreply.github.com>
2024-11-24 18:15:49 +0300

    first_project.py

--- first_project.py
+++ first_project.py
@@ -11,6 +11,18 @@ def generate_password(lenght=12):
 # Пример использования
 password = generate_password(12)
 print("Сгенерированный пароль:" , password)
+if __name__ == '__main__':
+    # Ввод длины пароля пользователем
+    try:
+        user_length = int(input("Введите длину пароля (не менее 6 символов): "))
+        if user_length < 6:
+            print("Длина пароля должна быть не менее 6 символов.")
+        else:
+            password = generate_password(user_length)
+            print('Сгенерированный пароль:', password)
+    except ValueError:
+        print("Пожалуйста, введите целое число.")
+
 
 
 
